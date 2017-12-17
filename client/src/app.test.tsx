64448fd727ca81d9b6ec044328c3c229cbe599ed
Charles Prakash Dasari <prakashd@konube.com>
2017-12-16 22:35:09 -0800

    Adding mocking for API calls

--- client/src/app.test.tsx
+++ client/src/app.test.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '../src/app';
+import * as fetchMock from 'fetch-mock';
 
 describe('main app module', () =>
   it('renders without crashing', () => {
+    fetchMock.mock('/api/v1/todos/', new Promise(resolve => resolve({
+      status: 200,
+      body: [],
+      sendAsJson: true
+    })));
+
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+
+    fetchMock.restore();
   }));
